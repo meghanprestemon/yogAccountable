5888b3fc47818ccf49b8260e07327ff219b5ffd0
Meghan Prestemon <m.m.hares@gmail.com>
2017-08-16 10:30:12 -0700

    Start creating /login route

---
+++ repositories/users_repo.js
@@ -0,0 +1,9 @@
+const knex = require('../knex');
+
+class UserRepository {
+  getUserData(email) {
+    return knex('users')
+      .where({ email })
+      .first();
+  }
+}
--- routes/users.js
+++ routes/users.js
@@ -7,3 +7,33 @@ const router = express.Router();
 const userRepo = new UserRepository();
 
 const saltRounds = 10;
+
+function verifyLoginCredentials(req, res) {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if (email && password) {
+    next();
+    return;
+  } else if (!email) {
+    res.status(400).send({field: 'email', error: 'undefined'});
+  } else if (!password) {
+    res.status(400).send({field: 'password', error: 'undefined'});
+  } else {
+    res.status(400).send({field: 'unspecified', error: 'unspecified'});
+  }
+}
+
+router.post('/login', verifyLoginCredentials, (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  let userId;
+
+  userRepo.getUserData(username)
+    .then(userData => {
+      
+    })
+
+});
+
+module.exports = router;
