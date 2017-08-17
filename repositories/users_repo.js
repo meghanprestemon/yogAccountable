ef0e318ea5cb3aae1c5a9213fca9594d961ef3cc
Meghan Prestemon <m.m.hares@gmail.com>
2017-08-17 13:54:09 -0700

    Complete login route

--- repositories/users_repo.js
+++ repositories/users_repo.js
@@ -1,9 +1,9 @@
-// const knex = require('../knex');
-//
-// class UserRepository {
-//   getUserData(email) {
-//     return knex('users')
-//       .where({ email })
-//       .first();
-//   }
-// }
+const knex = require('../knex');
+
+class UserRepository {
+  getUserData(email) {
+    return knex('users')
+      .where({ email })
+      .first();
+  }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,16 @@
-// const express = require('express');
-// const bcrypt = require('bcrypt');
-// const UserRepository = require('../repositories/users_repo');
-// const { camelizeKeys, decamelizeKeys } = require('humps');
-//
-// const router = express.Router();
-// const userRepo = new UserRepository();
-//
-// const saltRounds = 10;
-//
+const express = require('express');
+const bcrypt = require('bcrypt');
+const UserRepository = require('../repositories/users_repo');
+const { camelizeKeys, decamelizeKeys } = require('humps');
+
+const router = express.Router();
+const userRepo = new UserRepository();
+
+const saltRounds = 10;
+
+//NOTE do i need this? don't the first 2 steps of the /login route take care of this?
+        // i.e. !userData and !success
+
 // function verifyLoginCredentials(req, res) {
 //   let username = req.body.username;
 //   let password = req.body.password;
@@ -23,17 +26,42 @@
 //     res.status(400).send({field: 'unspecified', error: 'unspecified'});
 //   }
 // }
-//
-// router.post('/login', verifyLoginCredentials, (req, res) => {
-//   let username = req.body.username;
-//   let password = req.body.password;
-//   let userId;
-//
-//   userRepo.getUserData(username)
-//     .then(userData => {
-//
-//     })
-//
-// });
-//
-// module.exports = router;
+
+router.post('/login', verifyLoginCredentials, (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  let userId;
+
+  userRepo.getUserData(username)
+    .then(userData => {
+      if(!userData) {
+        res.header('Content-Type', 'text/plain');
+        return res.status(400).send({field: 'login', error: 'not found'});
+      }
+      userId = userData.id;
+      return bcrypt.compare(password, userData.password);
+    })
+    .then(success => {
+      if (!success) {
+        throw new Error('UNSUCCESSFUL_LOGIN');
+      }
+      const jwtPayload = {
+        iss: 'yogAccountable',
+        sub: {
+          id: userId
+        }
+      };
+      const token = jwt.sign(jwtPayload, process.env.JWT_KEY);
+      res.cookie('token', token, {httpOnly: true}).status(200).send({login: 'success'})
+    })
+    .catch(err => {
+      if (err.message === 'UNSUCCESSFUL_LOGIN') {
+        res.status(400).send({field: 'login', error: 'not found'});
+        return;
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.status(500).send(err);
+    });
+});
+
+module.exports = router;
